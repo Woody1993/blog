d0e34e25d9fd20575ad3a3aca6f296f775fb78fa
Woody <245195432@qq.com>
2019-03-05 15:10:06 +0800

    【2019-03-05】 1.模块化框架优化获取当前执行的script标签方法

--- test/mode.js
+++ test/mode.js
@@ -6,62 +6,36 @@
 		app: {}
 	};
 
-	function getCurrentScript() {
-		//取得正在解析的script节点
-		if(d.currentScript) { //firefox 4+
+	var crtDom = function() {
+		if (d.currentScript) {
 			return d.currentScript;
 		}
-		// 参考 https://github.com/samyk/jiagra/blob/master/jiagra.js
-		var stack;
-		try {
-			a.b.c(); //强制报错,以便捕获e.stack
-		} catch(e) {//safari的错误对象只有line,sourceId,sourceURL
-			stack = e.stack;
-			if(!stack && window.opera){
-				//opera 9没有e.stack,但有e.Backtrace,但不能直接取得,需要对e对象转字符串进行抽取
-				stack = (String(e).match(/of linked script \S+/g) || []).join(" ");
-			}
-		}
-		if(stack) {
-			console.log(stack)
-			/**e.stack最后一行在所有支持的浏览器大致如下:
-			*chrome23:
-			* at http://113.93.50.63/data.js:4:1
-			*firefox17:
-			*@http://113.93.50.63/query.js:4
-			*opera12:
-			*@http://113.93.50.63/data.js:4
-			*IE10:
-			*  at Global code (http://113.93.50.63/data.js:4:1)
-			*/
-			stack = stack.split( /[@ ]/g).pop();//取得最后一行,最后一个空格或@之后的部分
-			stack = stack[0] == "(" ? stack.slice(1,-1) : stack;
-			return stack.replace(/(:\d+)?:\d+$/i, "");//去掉行号与或许存在的出错字符起始位置
-		}
-		var nodes = head.getElementsByTagName("script"); //只在head标签中寻找
-		for(var i = 0, node; node = nodes[i++];) {
-			if(node.readyState === "interactive") {
-				return node;
-			}
-		}
-	}
 
-	var crtDom = function() {
-		return d.currentScript ? d.currentScript : function() {
-			for (var e, o = d.scripts, n = o.length - 1, r = n; r > 0; r--)
-				if ("interactive" === o[r].readyState) {
-					e = o[r];
-					break
-				}
-			return e || o[n]
-		}();
+		if (d.scripts[0].readyState === undefined) {
+			var stack;
+			try {
+				undefined.undefined;
+			} catch(e) {
+				stack = !e.stack && window.opera ? (String(e).match(/of linked script \S+/g) || []).join(" ") : e.stack;
+				stack && (
+					stack = stack.split( /[@ ]/g).pop(),
+					stack = stack[0] == "(" ? stack.slice(1,-1) : stack,
+					stack = stack.replace(/(:\d+)?:\d+$/i, "")
+				);
+			}
+		}
+		
+		for (var e, o = d.scripts, n = o.length - 1, r = n; r > 0; r--)
+			if ("interactive" === o[r].readyState || stack && o[r].src == stack) {
+				e = o[r];
+				break
+			}
+		return e || o[n]
 	};
 
 	var baseUrl = function() {
 		return src = crtDom().src, src.substring(0, src.lastIndexOf("/") + 1);
 	}();
-
-	console.log(baseUrl);
 
 	var paths = {
 		jquery: "script/jquery",
@@ -75,8 +49,6 @@
 	var createPath = function(u, type) {
 		return (absPath(u) ? "" : baseUrl) + u + (type ? (type == 'js' ? (/\.js$/.test(u) ? "" : ".js") : (/\.css$/.test(u) ? "" : ".css")) : '');
 	}
-
-	var a = "undefined" != typeof opera && "[object Opera]" === opera.toString();
 
 	w.d = {
 		require: function(modules, callback, exports) {
@@ -133,7 +105,7 @@
 				head.appendChild(el);
 				el.addEventListener ||
 				!el.attachEvent || 
-				el.attachEvent.toString && el.attachEvent.toString().indexOf("[native code") < 0 || a ? el.addEventListener("load", function(e) {
+				el.attachEvent.toString && el.attachEvent.toString().indexOf("[native code") < 0 ? el.addEventListener("load", function(e) {
 					onFileLoad(e);
 				}, !1) : el.attachEvent("onreadyftatechange", function(e) {
 					onFileLoad(e);
@@ -151,7 +123,7 @@
 
 		define: function(deps, factory) {
 			var that = this,
-				module = getCurrentScript().getAttribute("module");
+				module = crtDom().getAttribute("module");
 			typeof deps === 'function' && (
 				factory = deps,
 				deps = []
